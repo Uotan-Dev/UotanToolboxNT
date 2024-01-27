afb9947e9d1de0b462e6615eb0808e67d97eb8a5
yuzh <46953451+yuzh0816@users.noreply.github.com>
2024-01-28 00:47:27 +0800

    feat: Homepage rebuild

--- SukiUI.Demo/Features/Splash/SplashView.axaml.cs
+++ SukiUI.Demo/Features/Splash/SplashView.axaml.cs
@@ -1,4 +1,6 @@
 using Avalonia.Controls;
+using Avalonia.Input;
+using Avalonia.Interactivity;
 using Avalonia.Threading;
 
 namespace SukiUI.Demo.Features.Splash;
@@ -7,13 +9,15 @@ public partial class SplashView : UserControl
 {
     public SplashView()
     {
-        SukiTheme.GetInstance().OnBaseThemeChanged += _ =>
-        {
-            Dispatcher.UIThread.Post(() =>
-            {
-                TextBlockWithInline.InvalidateVisual();
-            });
-        };
         InitializeComponent();
+    }
+
+    private async void CopyButton_OnClick(object? sender, RoutedEventArgs args)
+    {
+        Button button  = (Button)sender!;
+        var clipboard = TopLevel.GetTopLevel(this)?.Clipboard;
+        var dataObject = new DataObject();
+        dataObject.Set(DataFormats.Text, button.Content.ToString());
+        await clipboard.SetDataObjectAsync(dataObject);
     }
 }
--- SukiUI.Demo/Features/Splash/SplashViewModel.cs
+++ SukiUI.Demo/Features/Splash/SplashViewModel.cs
@@ -1,12 +1,16 @@
+using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
 using Material.Icons;
 using SukiUI.Demo.Features.Dashboard;
 using SukiUI.Demo.Services;
+using System.ComponentModel.DataAnnotations;
 
 namespace SukiUI.Demo.Features.Splash;
 
 public partial class SplashViewModel(PageNavigationService nav) : DemoPageBase("Welcome", MaterialIconKind.Hand, int.MinValue)
 {
+    [ObservableProperty][Range(0d, 100d)] private double _progressValue = 50;
+
     [RelayCommand]
     public void OpenDashboard()
     {

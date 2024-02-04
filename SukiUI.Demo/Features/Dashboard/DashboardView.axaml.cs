b9c02c9130d45c2665c955824d431f3146badd3f
yuzh <46953451+yuzh0816@users.noreply.github.com>
2024-02-04 10:14:52 +0800

    ex: Add samples of file picker

--- SukiUI.Demo/Features/Dashboard/DashboardView.axaml.cs
+++ SukiUI.Demo/Features/Dashboard/DashboardView.axaml.cs
@@ -1,4 +1,8 @@
 using Avalonia.Controls;
+using Avalonia.Interactivity;
+using Avalonia.Platform.Storage;
+using System;
+using System.IO;
 
 namespace SukiUI.Demo.Features.Dashboard;
 
@@ -8,4 +12,25 @@ public partial class DashboardView : UserControl
     {
         InitializeComponent();
     }
+
+    private async void OpenFileButton_Clicked(object sender, RoutedEventArgs args)
+    {
+        var topLevel = TopLevel.GetTopLevel(this);
+        var files = await topLevel.StorageProvider.OpenFilePickerAsync(new FilePickerOpenOptions
+        {
+            Title = "Open File",
+            AllowMultiple = false
+        });
+
+        if (files.Count >= 1)
+        {
+            await using var stream = await files[0].OpenReadAsync();
+            using var streamReader = new StreamReader(stream);
+            // File content
+            var fileContent = await streamReader.ReadToEndAsync();
+            // File path
+            var filePath = files[0].Path;
+        }
+    }
+
 }

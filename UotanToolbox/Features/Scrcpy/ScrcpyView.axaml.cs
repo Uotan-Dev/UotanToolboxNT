b2fd5dbf3307bc80a690758803df7a78752d1b6b
yuzh <46953451+yuzh0816@users.noreply.github.com>
2024-02-25 09:48:02 +0800

    feat: Add more function of scrcpy

--- UotanToolbox/Features/Scrcpy/ScrcpyView.axaml.cs
+++ UotanToolbox/Features/Scrcpy/ScrcpyView.axaml.cs
@@ -11,4 +11,18 @@ public partial class ScrcpyView : UserControl
     {
         InitializeComponent();
     }
+
+    private async void OpenFolderButton_Clicked(object sender, RoutedEventArgs args)
+    {
+        var topLevel = TopLevel.GetTopLevel(this);
+        var files = await topLevel.StorageProvider.OpenFolderPickerAsync(new FolderPickerOpenOptions
+        {
+            Title = "Open Folder",
+            AllowMultiple = false
+        });
+        if (files.Count >= 1)
+        {
+            RecordFolder.Text = StringHelper.FilePath(files[0].Path.ToString());
+        }
+    }
 }
--- UotanToolbox/Features/Scrcpy/ScrcpyViewModel.cs
+++ UotanToolbox/Features/Scrcpy/ScrcpyViewModel.cs
@@ -3,6 +3,7 @@ using CommunityToolkit.Mvvm.Input;
 using Material.Icons;
 using ReactiveUI;
 using System;
+using System.ComponentModel.DataAnnotations;
 using System.Diagnostics;
 using System.Threading.Tasks;
 using UotanToolbox.Common;
@@ -11,10 +12,15 @@ namespace UotanToolbox.Features.Scrcpy;
 
 public partial class ScrcpyViewModel : MainPageBase
 {
-    [ObservableProperty] private bool _windowFixed = false, _computerControl = true, _fullScreen = false, _showBorder = true,
-                        _showTouch = true, _renderAllFrame = false, _closeScreen = false, _screenAwake = false, _screenAwakeStatus = true;
+    [ObservableProperty] private bool _recordScreen = false, _windowFixed = false, _computerControl = true, _fullScreen = false, _showBorder = true,
+                        _showTouch = true, _closeScreen = false, _screenAwake = false, _screenAwakeStatus = true;
     [ObservableProperty] private bool _isConnected;
-    [ObservableProperty] private string _windowTitle;
+    [ObservableProperty] private string _windowTitle, _recordFolder;
+
+    [ObservableProperty][Range(0d, 10d)] private double _bitRate = 8;
+    [ObservableProperty][Range(0d, 180d)] private double _frameRate = 60;
+    [ObservableProperty][Range(0d, 2048d)] private double _sizeResolution = 0;
+
     public ScrcpyViewModel() : base("Scrcpy", MaterialIconKind.CellphoneLink, -500)
     {
         this.WhenAnyValue(x => x.ComputerControl)
@@ -36,6 +42,18 @@ public partial class ScrcpyViewModel : MainPageBase
         return Task.Run(async () =>
         {
             string arg = $"-s {Global.thisdevice} ";
+            if (RecordScreen)
+            {
+                if(RecordFolder != null)
+                {
+                    DateTime now = DateTime.Now;
+                    string formattedDateTime = now.ToString("yyyy-MM-dd-HH-mm-ss");
+                    arg += $"--record {RecordFolder}/{Global.thisdevice}-{formattedDateTime}.mp4 ";
+                }
+            }
+            arg += $"--video-bit-rate {BitRate}M --max-fps {FrameRate} ";
+            if (SizeResolution != 0) arg += $"--max-size {SizeResolution} ";
+
             if (WindowTitle != "" && WindowTitle !=null)
             {
                 arg += $"--window-title {WindowTitle} ";
@@ -44,7 +62,6 @@ public partial class ScrcpyViewModel : MainPageBase
             if (FullScreen) arg += "--fullscreen ";
             if (!ShowBorder) arg += "--window-borderless ";
             if (ShowTouch) arg += "--show-touches ";
-            if (RenderAllFrame) arg += "--render-expired-frames ";
             if (!ComputerControl) arg += "--no-control ";
             if (CloseScreen) arg += "--turn-screen-off ";
             if (ScreenAwake) arg += "--stay-awake ";

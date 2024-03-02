9af309885c386bf2edde740125d71b9358e1d7e2
yuzh <46953451+yuzh0816@users.noreply.github.com>
2024-03-02 20:36:11 +0800

    feat: Add AppManager.

---
+++ UotanToolbox/Features/Appmgr/AppmgrViewModel.cs
@@ -0,0 +1,10 @@
+﻿using Material.Icons;
+
+namespace UotanToolbox.Features.Appmgr;
+
+public partial class AppmgrViewModel : MainPageBase
+{
+    public AppmgrViewModel() : base("应用管理", MaterialIconKind.ViewGridPlusOutline, -700)
+    {
+    }
+}

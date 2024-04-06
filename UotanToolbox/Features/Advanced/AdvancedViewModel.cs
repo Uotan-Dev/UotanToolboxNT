faaa401d3f10ee704b53f3d39dfac2e994f8fcb3
yuzh <46953451+yuzh0816@users.noreply.github.com>
2024-04-06 14:11:45 +0800

    feat: Create Advanced page
    With a new card QCN.

---
+++ UotanToolbox/Features/Advanced/AdvancedViewModel.cs
@@ -0,0 +1,45 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using Material.Icons;
+using ReactiveUI;
+using System;
+using System.ComponentModel.DataAnnotations;
+using System.Threading.Tasks;
+using UotanToolbox.Common;
+
+namespace UotanToolbox.Features.Advanced;
+
+public partial class AdvancedViewModel : MainPageBase
+{
+    [ObservableProperty] private string _qcnFile;
+
+    public AdvancedViewModel() : base("高级", MaterialIconKind.WrenchCogOutline, -300)
+    {
+    }
+
+    [RelayCommand]
+    private async Task WriteQcn()
+    {
+        // 写入QCN文件
+    }
+    [RelayCommand]
+    private async Task BackupQcn()
+    {
+        // 备份QCN文件
+    }
+    [RelayCommand]
+    private async Task OpenBackup()
+    {
+        // 备份QCN文件
+    }
+    [RelayCommand]
+    private async Task Enable901d()
+    {
+        // 开启901D
+    }
+    [RelayCommand]
+    private async Task Enable9091()
+    {
+        // 开启9091
+    }
+}

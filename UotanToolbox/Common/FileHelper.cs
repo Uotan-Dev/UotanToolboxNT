a42ae3f8c50861a03135cf4df4780eca1c4a1326
mujianwu <lrh15563910083@163.com>
2024-05-29 20:31:30 +0800

    添加文件操作类

---
+++ UotanToolbox/Common/FileHelper.cs
@@ -0,0 +1,29 @@
+﻿using SkiaSharp;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UotanToolbox.Common
+{
+    internal class FileHelper
+    {
+        public static void CopyDirectory(string srcPath, string aimPath)
+        {
+            try
+            {
+                string[] fileList = Directory.GetFiles(srcPath);
+                foreach (string file in fileList)
+                {
+                    File.Copy(file, aimPath + Path.GetFileName(file), true);
+                }
+            }
+            catch
+            {
+                throw;
+            }
+        }
+    }
+}

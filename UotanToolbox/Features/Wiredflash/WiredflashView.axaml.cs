fce9d7541aaab21fedc80d8cc3475a982ea47196
yuzh <46953451+yuzh0816@users.noreply.github.com>
2024-06-01 11:45:27 +0800

    feat: Add Wiredflash module

---
+++ UotanToolbox/Features/Wiredflash/WiredflashView.axaml.cs
@@ -0,0 +1,28 @@
+﻿using Avalonia.Controls;
+using Avalonia.Interactivity;
+using Avalonia.Platform.Storage;
+using UotanToolbox.Common;
+
+namespace UotanToolbox.Features.Wiredflash;
+
+public partial class WiredflashView : UserControl
+{
+    public WiredflashView()
+    {
+        InitializeComponent();
+    }
+
+    private async void OpenFastbootFile(object sender, RoutedEventArgs args)
+    {
+        var topLevel = TopLevel.GetTopLevel(this);
+        var files = await topLevel.StorageProvider.OpenFilePickerAsync(new FilePickerOpenOptions
+        {
+            Title = "Open Fastboot File",
+            AllowMultiple = false
+        });
+        if (files.Count >= 1)
+        {
+            FastbootFile.Text = StringHelper.FilePath(files[0].Path.ToString());
+        }
+    }
+}

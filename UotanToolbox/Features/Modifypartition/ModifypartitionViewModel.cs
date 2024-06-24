c8ccb4b884acc3f0a34ee2b60a80b77fa56ea680
mujianwu <lrh15563910083@163.com>
2024-06-25 00:04:48 +0800

    调整修改分区UI

--- UotanToolbox/Features/Modifypartition/ModifypartitionViewModel.cs
+++ UotanToolbox/Features/Modifypartition/ModifypartitionViewModel.cs
@@ -8,24 +8,8 @@ namespace UotanToolbox.Features.Modifypartition;
 
 public partial class ModifypartitionViewModel : MainPageBase
 {
-    public AvaloniaList<DataGridContentViewModel> DataGridContent { get; } = [];
-
     private static string GetTranslation(string key) => FeaturesHelper.GetTranslation(key);
     public ModifypartitionViewModel() : base(GetTranslation("Sidebar_ModifyPartition"), MaterialIconKind.WrenchCogOutline, -400)
     {
-        DataGridContent.AddRange(Enumerable.Range(1, 50).Select(x => new DataGridContentViewModel(x)));
     }
-
-    public partial class DataGridContentViewModel(int value) : ObservableObject
-    {
-        [ObservableProperty]
-        private string _idColumn = $"Content {value}",
-            _startpointColumn = $"Content {value}",
-            _endpointColumn = $"Content {value}",
-            _sizeColumn = $"Content {value}",
-            _formatColumn = $"Content {value}",
-            _nameColumn = $"Content {value}",
-            _signColumn = $"Content {value}";
-    }
-
 }

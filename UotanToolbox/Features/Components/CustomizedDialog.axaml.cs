3da8960bec85860200d26a962c229a5853c5f62d
yuzh <46953451+yuzh0816@users.noreply.github.com>
2024-07-05 16:22:45 +0800

    fix: Change logic of update dialog

---
+++ UotanToolbox/Features/Components/CustomizedDialog.axaml.cs
@@ -0,0 +1,45 @@
+﻿using Avalonia.Controls;
+using Avalonia.Interactivity;
+using Avalonia.Threading;
+using SukiUI.Controls;
+
+namespace UotanToolbox.Features.Components
+{
+    public partial class CustomizedDialog : UserControl
+    {
+        public bool Result { get; private set; }
+
+        public CustomizedDialog() { }
+        public CustomizedDialog(string DialogTitle, string DialogContent)
+        {
+            Dispatcher.UIThread.Post(() =>
+            {
+                var titleTextBlock = this.FindControl<TextBlock>("DialogTitle");
+                if (titleTextBlock != null)
+                {
+                    titleTextBlock.Text = DialogTitle;
+                }
+
+                var dialogueTextBlock = this.FindControl<TextBlock>("DialogContent");
+                if (dialogueTextBlock != null)
+                {
+                    dialogueTextBlock.Text = DialogContent;
+                }
+            });
+            Result = false;
+            InitializeComponent();
+        }
+
+        private void Cancel_OnClick(object sender, RoutedEventArgs e)
+        {
+            Result = false;
+            SukiHost.CloseDialog();
+        }
+
+        private void Confirm_OnClick(object sender, RoutedEventArgs e)
+        {
+            Result = true;
+            SukiHost.CloseDialog();
+        }
+    }
+}

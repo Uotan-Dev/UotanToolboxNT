844b6281b2a9cc7a38a128266d6c0a9ed7d4ed5e
mujianwu <lrh15563910083@163.com>
2024-07-16 19:12:52 +0800

    Revert "锁定Windows窗体"
    This reverts commit ee711b00548db3dec88b52d10f2bb108904576f1.

--- SukiUI/Controls/SukiWindow.axaml.cs
+++ SukiUI/Controls/SukiWindow.axaml.cs
@@ -11,7 +11,6 @@ using Avalonia.Collections;
 using Avalonia.Controls.ApplicationLifetimes;
 using Avalonia.Interactivity;
 using SukiUI.Enums;
-using System.Runtime.InteropServices;
 
 namespace SukiUI.Controls;
 
@@ -21,24 +20,6 @@ public class SukiWindow : Window
 
     public static readonly StyledProperty<double> TitleFontSizeProperty =
         AvaloniaProperty.Register<SukiWindow, double>(nameof(TitleFontSize), defaultValue: 13);
-
-    public SukiWindow()
-    {
-        MenuItems = new AvaloniaList<MenuItem>();
-        SetSystemDecorationsBasedOnPlatform();
-    }
-
-    private void SetSystemDecorationsBasedOnPlatform()
-    {
-        if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
-        {
-            this.SystemDecorations = SystemDecorations.None;
-        }
-        else
-        {
-            this.SystemDecorations = SystemDecorations.BorderOnly;
-        }
-    }
 
     public double TitleFontSize
     {
@@ -192,6 +173,11 @@ public class SukiWindow : Window
     {
         get => GetValue(RightWindowTitleBarControlsProperty);
         set => SetValue(RightWindowTitleBarControlsProperty, value);
+    }
+
+    public SukiWindow()
+    {
+        MenuItems = new AvaloniaList<MenuItem>();
     }
 
     private IDisposable? _subscriptionDisposables;

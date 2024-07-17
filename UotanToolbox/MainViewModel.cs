32fd02edb3104cd3fdfd9a2fec978cd349743e68
mujianwu <lrh15563910083@163.com>
2024-07-17 17:52:21 +0800

    Revert "feat: Change background style to Avalonia default."
    This reverts commit 3bb3d06bd3de9bd3894ac12952083da871a00120.

--- UotanToolbox/MainViewModel.cs
+++ UotanToolbox/MainViewModel.cs
@@ -26,9 +26,12 @@ public partial class MainViewModel : ObservableObject
 
     public IAvaloniaReadOnlyList<SukiColorTheme> Themes { get; }
 
+    public IAvaloniaReadOnlyList<SukiBackgroundStyle> BackgroundStyles { get; }
+
     [ObservableProperty] private ThemeVariant _baseTheme;
     [ObservableProperty] private bool _animationsEnabled;
     [ObservableProperty] private MainPageBase _activePage;
+    [ObservableProperty] private SukiBackgroundStyle _backgroundStyle = SukiBackgroundStyle.Gradient;
     [ObservableProperty] private string _customShaderFile;
     [ObservableProperty] private bool _transitionsEnabled;
     [ObservableProperty] private double _transitionTime;
@@ -46,6 +49,13 @@ public partial class MainViewModel : ObservableObject
     {
         Status = "--"; CodeName = "--"; BLStatus = "--"; VABStatus = "--";
         DemoPages = new AvaloniaList<MainPageBase>(demoPages.OrderBy(x => x.Index).ThenBy(x => x.DisplayName));
+        _theming = (SettingsViewModel)DemoPages.First(x => x is SettingsViewModel);
+        _theming.BackgroundStyleChanged += style => BackgroundStyle = style;
+        _theming.BackgroundAnimationsChanged += enabled => AnimationsEnabled = enabled;
+        _theming.CustomBackgroundStyleChanged += shader => CustomShaderFile = shader;
+        _theming.BackgroundTransitionsChanged += enabled => TransitionsEnabled = enabled;
+
+        BackgroundStyles = new AvaloniaList<SukiBackgroundStyle>(Enum.GetValues<SukiBackgroundStyle>());
         _theme = SukiTheme.GetInstance();
         nav.NavigationRequested += t =>
         {

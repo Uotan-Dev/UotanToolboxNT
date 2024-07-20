07f17d3dfe605a12b9cb0da651798e696a571c7f
yuzh <46953451+yuzh0816@users.noreply.github.com>
2024-07-20 16:54:08 +0800

    Modify some code

--- SukiUI/Controls/SukiWindow.axaml.cs
+++ SukiUI/Controls/SukiWindow.axaml.cs
@@ -1,40 +1,25 @@
 using Avalonia;
-using Avalonia.Collections;
 using Avalonia.Controls;
-using Avalonia.Controls.ApplicationLifetimes;
 using Avalonia.Controls.Primitives;
 using Avalonia.Input;
-using Avalonia.Interactivity;
 using Avalonia.Media;
-using SukiUI.Enums;
+using Avalonia.Threading;
 using System;
 using System.Reactive;
 using System.Reactive.Linq;
-using System.Runtime.InteropServices;
+using System.Reflection;
+using Avalonia.Collections;
+using Avalonia.Controls.ApplicationLifetimes;
+using Avalonia.Interactivity;
+using SukiUI.Enums;
+using SukiUI.Utilities;
 
 namespace SukiUI.Controls;
 
 public class SukiWindow : Window
 {
-    public SukiWindow()
-    {
-        MenuItems = new AvaloniaList<MenuItem>();
-        SetSystemDecorationsBasedOnPlatform();
-    }
-
-    private void SetSystemDecorationsBasedOnPlatform()
-    {
-        if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
-        {
-            this.SystemDecorations = SystemDecorations.None;
-        }
-        else
-        {
-            this.SystemDecorations = SystemDecorations.BorderOnly;
-        }
-    }
-
     protected override Type StyleKeyOverride => typeof(SukiWindow);
+
     public static readonly StyledProperty<double> TitleFontSizeProperty =
         AvaloniaProperty.Register<SukiWindow, double>(nameof(TitleFontSize), defaultValue: 13);
 
@@ -130,9 +115,9 @@ public class SukiWindow : Window
 
     public static readonly StyledProperty<SukiBackgroundStyle> BackgroundStyleProperty =
         AvaloniaProperty.Register<SukiWindow, SukiBackgroundStyle>(nameof(BackgroundStyle),
-            defaultValue: SukiBackgroundStyle.Waves);
+            defaultValue: SukiBackgroundStyle.Bubble);
 
-
+    
     /// <inheritdoc cref="SukiBackground.Style"/>
     public SukiBackgroundStyle BackgroundStyle
     {
@@ -153,17 +138,17 @@ public class SukiWindow : Window
     public static readonly StyledProperty<string?> BackgroundShaderCodeProperty =
         AvaloniaProperty.Register<SukiWindow, string?>(nameof(BackgroundShaderCode));
 
-
+    
     /// <inheritdoc cref="SukiBackground.ShaderCode"/>
     public string? BackgroundShaderCode
     {
         get => GetValue(BackgroundShaderCodeProperty);
         set => SetValue(BackgroundShaderCodeProperty, value);
     }
-
+    
     public static readonly StyledProperty<bool> BackgroundTransitionsEnabledProperty =
         AvaloniaProperty.Register<SukiBackground, bool>(nameof(BackgroundTransitionsEnabled), defaultValue: false);
-
+    
     /// <inheritdoc cref="SukiBackground.TransitionsEnabled"/>
     public bool BackgroundTransitionsEnabled
     {
@@ -173,12 +158,28 @@ public class SukiWindow : Window
 
     public static readonly StyledProperty<double> BackgroundTransitionTimeProperty =
         AvaloniaProperty.Register<SukiBackground, double>(nameof(BackgroundTransitionTime), defaultValue: 1.0);
-
+    
     /// <inheritdoc cref="SukiBackground.TransitionTime"/>
     public double BackgroundTransitionTime
     {
         get => GetValue(BackgroundTransitionTimeProperty);
         set => SetValue(BackgroundTransitionTimeProperty, value);
+    }
+
+    public static readonly StyledProperty<Avalonia.Controls.Controls> RightWindowTitleBarControlsProperty = AvaloniaProperty.Register<SukiWindow, Avalonia.Controls.Controls>(nameof(RightWindowTitleBarControls), defaultValue: new Avalonia.Controls.Controls());
+
+    /// <summary>
+    /// Controls that are displayed on the right side of the title bar, to the left of the normal window control buttons. (Displays provided controls right-to-left)
+    /// </summary>
+    public Avalonia.Controls.Controls RightWindowTitleBarControls
+    {
+        get => GetValue(RightWindowTitleBarControlsProperty);
+        set => SetValue(RightWindowTitleBarControlsProperty, value);
+    }
+
+    public SukiWindow()
+    {
+        MenuItems = new AvaloniaList<MenuItem>();
     }
 
     private IDisposable? _subscriptionDisposables;
@@ -197,36 +198,102 @@ public class SukiWindow : Window
         }
     }
 
+
     protected override void OnApplyTemplate(TemplateAppliedEventArgs e)
     {
         base.OnApplyTemplate(e);
-        var stateObs = this.GetObservable(WindowStateProperty)
-            .Select(windowState => windowState == WindowState.Maximized ? Unit.Default : Unit.Default);
-        // Create handlers for buttons
-        if (e.NameScope.Get<Button>("PART_MaximizeButton") is { } maximize)
+
+        _subscriptionDisposables = this.GetObservable(WindowStateProperty)
+            .Do(OnWindowStateChanged)
+            .Select(_ => Unit.Default).ObserveOn(new AvaloniaSynchronizationContext())
+            .Subscribe();
+        try
         {
-            maximize.Click += (_, _) =>
+            // Create handlers for buttons
+            if (e.NameScope.Get<Button>("PART_MaximizeButton") is { } maximize)
             {
-                if (!CanResize) return;
-                WindowState = WindowState == WindowState.Maximized
-                    ? WindowState.Normal
-                    : WindowState.Maximized;
-            };
+                maximize.Click += OnMaximizeButtonClicked;
+                bool pointerOnMaxButton = false;
+                var  setter             = typeof(Button).GetProperty("IsPointerOver");
+
+                var proc = (IntPtr hWnd, uint msg, IntPtr wParam, IntPtr lParam, ref bool handled) =>
+                {
+                    switch (msg)
+                    {
+                        case 533:
+                            if (!pointerOnMaxButton) break;
+                            if (!CanResize) break;
+                            WindowState = WindowState == WindowState.Maximized
+                                ? WindowState.Normal
+                                : WindowState.Maximized;
+                            break;
+                        case 0x0084:
+                            var point = new PixelPoint(
+                                (short)(ToInt32(lParam) & 0xffff),
+                                (short)(ToInt32(lParam) >> 16));
+                            var buttonLeftTop = maximize.PointToScreen(new(0, 0));
+                            var x             = (buttonLeftTop.X - point.X)         / RenderScaling;
+                            var y             = (point.Y         - buttonLeftTop.Y) / RenderScaling;
+                            if (new Rect(0, 0,
+                                    maximize.DesiredSize.Width,
+                                    maximize.DesiredSize.Height)
+                                .Contains(new Point(x, y)))
+                            {
+                                setter?.SetValue(maximize, true);
+                                pointerOnMaxButton     = true;
+                                handled                = true;
+                                return (IntPtr)9;
+                            }
+
+                            pointerOnMaxButton = false;
+                            setter?.SetValue(maximize, false);
+                            break;
+                    }
+
+                    return IntPtr.Zero;
+                    
+                    static int ToInt32(IntPtr ptr) => IntPtr.Size == 4 ? ptr.ToInt32() : (int)(ptr.ToInt64() & 0xffffffff);
+                };
+
+                
+
+                Win32Properties.AddWndProcHookCallback(this, new Win32Properties.CustomWndProcHookCallback(proc));
+            } 
+
+            if (e.NameScope.Get<Button>("PART_MinimizeButton") is { } minimize)
+                minimize.Click += (_, _) =>
+                {
+           
+                    WindowState = WindowState.Minimized;
+                   
+                };
+
+            if (e.NameScope.Get<Button>("PART_CloseButton") is { } close)
+                close.Click += (_, _) => Close();
+
+            if (e.NameScope.Get<GlassCard>("PART_TitleBarBackground") is { } titleBar)
+            {
+                titleBar.PointerPressed += OnTitleBarPointerPressed;
+                titleBar.DoubleTapped += OnMaximizeButtonClicked;
+            }
         }
-        if (e.NameScope.Get<Button>("PART_MinimizeButton") is { } minimize)
-            minimize.Click += (_, _) => WindowState = WindowState.Minimized;
-        if (e.NameScope.Get<Button>("PART_CloseButton") is { } close)
-            close.Click += (_, _) => Close();
-        if (e.NameScope.Get<GlassCard>("PART_TitleBarBackground") is { } titleBar)
-            titleBar.PointerPressed += OnTitleBarPointerPressed;
+        catch
+        {
+        }
+    }
+
+    private void OnMaximizeButtonClicked(object? sender, RoutedEventArgs args)
+    {
+        if (!CanResize) return;
+        WindowState = WindowState == WindowState.Maximized
+            ? WindowState.Normal
+            : WindowState.Maximized;
     }
 
     private void OnWindowStateChanged(WindowState state)
     {
         if (state == WindowState.FullScreen)
             CanResize = CanMove = false;
-        else
-            CanResize = CanMove = true;
     }
 
     private void OnTitleBarPointerPressed(object? sender, PointerPressedEventArgs e)

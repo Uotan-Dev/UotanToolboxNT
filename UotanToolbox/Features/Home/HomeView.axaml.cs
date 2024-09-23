c51877cc760c7ecd086f8b0d645745996f10370b
mujianwu <lrh15563910083@163.com>
2024-09-23 13:03:29 +0800

    调整UI

--- UotanToolbox/Features/Home/HomeView.axaml.cs
+++ UotanToolbox/Features/Home/HomeView.axaml.cs
@@ -1,9 +1,14 @@
 ﻿using Avalonia.Controls;
 using Avalonia.Input;
 using Avalonia.Interactivity;
+using Avalonia.Threading;
 using SukiUI.Controls;
 using SukiUI.Enums;
+using System;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
 using UotanToolbox.Common;
+using UotanToolbox.Features.Components;
 
 namespace UotanToolbox.Features.Home;
 
@@ -30,6 +35,89 @@ public partial class HomeView : UserControl
             if (clipboard != null)
                 await clipboard.SetDataObjectAsync(dataObject);
             await SukiHost.ShowToast(GetTranslation("Home_Copy"), "o(*≧▽≦)ツ", NotificationType.Success);
+        }
+    }
+
+    private async void OpenAFDI(object sender, RoutedEventArgs args)
+    {
+        if (Global.System == "Windows")
+        {
+            if (RuntimeInformation.OSArchitecture == Architecture.X64)
+            {
+                Process.Start(@"Drive\adb.exe");
+            }
+            else if (RuntimeInformation.OSArchitecture == Architecture.Arm64)
+            {
+                string drvpath = String.Format($"{Global.runpath}/Drive/adb/*.inf");
+                string shell = String.Format("/add-driver {0} /subdirs /install", drvpath);
+                string drvlog = await CallExternalProgram.Pnputil(shell);
+                FileHelper.Write($"{Global.log_path}/drive.txt", drvlog);
+                if (drvlog.Contains(GetTranslation("Basicflash_Success")))
+                {
+                    SukiHost.ShowDialog(new PureDialog(GetTranslation("Common_InstallSuccess")), allowBackgroundClose: true);
+                }
+                else
+                {
+                    SukiHost.ShowDialog(new PureDialog(GetTranslation("Common_InstallFailed")), allowBackgroundClose: true);
+                }
+            }
+        }
+        else
+        {
+            SukiHost.ShowDialog(new PureDialog(GetTranslation("Basicflash_NotUsed")), allowBackgroundClose: true);
+        }
+    }
+
+    private async void Open9008DI(object sender, RoutedEventArgs args)
+    {
+        if (Global.System == "Windows")
+        {
+            if (RuntimeInformation.OSArchitecture == Architecture.X64)
+            {
+                Process.Start(@"Drive\Qualcomm_HS-USB_Driver.exe");
+            }
+            else if (RuntimeInformation.OSArchitecture == Architecture.Arm64)
+            {
+                string drvpath = String.Format($"{Global.runpath}/drive/9008/*.inf");
+                string shell = String.Format("/add-driver {0} /subdirs /install", drvpath);
+                string drvlog = await CallExternalProgram.Pnputil(shell);
+                FileHelper.Write($"{Global.log_path}/drive.txt", drvlog);
+                if (drvlog.Contains(GetTranslation("Basicflash_Success")))
+                {
+                    SukiHost.ShowDialog(new PureDialog(GetTranslation("Common_InstallSuccess")), allowBackgroundClose: true);
+                }
+                else
+                {
+                    SukiHost.ShowDialog(new PureDialog(GetTranslation("Common_InstallFailed")), allowBackgroundClose: true);
+                }
+            }
+        }
+        else
+        {
+            SukiHost.ShowDialog(new PureDialog(GetTranslation("Basicflash_NotUsed")), allowBackgroundClose: true);
+        }
+    }
+
+    private async void OpenUSBP(object sender, RoutedEventArgs args)
+    {
+        if (Global.System == "Windows")
+        {
+            string cmd = @"drive\USB3.bat";
+            ProcessStartInfo cmdshell = null;
+            cmdshell = new ProcessStartInfo(cmd)
+            {
+                CreateNoWindow = true,
+                UseShellExecute = false
+            };
+            Process f = Process.Start(cmdshell);
+            await Dispatcher.UIThread.InvokeAsync(() =>
+            {
+                SukiHost.ShowDialog(new PureDialog(GetTranslation("Common_Execution")), allowBackgroundClose: true);
+            });
+        }
+        else
+        {
+            SukiHost.ShowDialog(new PureDialog(GetTranslation("Basicflash_NotUsed")), allowBackgroundClose: true);
         }
     }
 }
--- UotanToolbox/Features/Home/HomeViewModel.cs
+++ UotanToolbox/Features/Home/HomeViewModel.cs
@@ -200,26 +200,6 @@ public partial class HomeViewModel : MainPageBase
         }
     }
 
-    private async Task SystemControl(string shell)
-    {
-        if (await GetDevicesInfo.SetDevicesInfoLittle())
-        {
-            MainViewModel sukiViewModel = GlobalData.MainViewModelInstance;
-            if (sukiViewModel.Status == GetTranslation("Home_System"))
-            {
-                await CallExternalProgram.ADB($"-s {Global.thisdevice} {shell}");
-            }
-            else
-            {
-                SukiHost.ShowDialog(new PureDialog(GetTranslation("Common_OpenADB")), allowBackgroundClose: true);
-            }
-        }
-        else
-        {
-            SukiHost.ShowDialog(new PureDialog(GetTranslation("Common_NotConnected")), allowBackgroundClose: true);
-        }
-    }
-
     private async Task ADBControl(string shell)
     {
         if (await GetDevicesInfo.SetDevicesInfoLittle())
@@ -258,36 +238,6 @@ public partial class HomeViewModel : MainPageBase
         {
             SukiHost.ShowDialog(new PureDialog(GetTranslation("Common_NotConnected")), allowBackgroundClose: true);
         }
-    }
-
-
-    [RelayCommand]
-    public async Task Back() => await SystemControl("shell input keyevent 4");
-
-    [RelayCommand]
-    public async Task Home() => await SystemControl("shell input keyevent 3");
-
-    [RelayCommand]
-    public async Task Mul() => await SystemControl("shell input keyevent 187");
-
-    [RelayCommand]
-    public async Task Lock() => await SystemControl("shell input keyevent 26");
-
-    [RelayCommand]
-    public async Task VolU() => await SystemControl("shell input keyevent 24");
-
-    [RelayCommand]
-    public async Task VolD() => await SystemControl("shell input keyevent 25");
-
-    [RelayCommand]
-    public async Task Mute() => await SystemControl("shell input keyevent 164");
-
-    [RelayCommand]
-    public async Task SC()
-    {
-        string pngname = String.Format($"{DateAndTime.Now:yyyy-MM-dd_HH-mm-ss}");
-        await SystemControl($"shell /system/bin/screencap -p /sdcard/{pngname}.png");
-        await SukiHost.ShowToast(GetTranslation("Home_Succeeded"), $"{GetTranslation("Home_Saved")} {pngname}.png {GetTranslation("Home_ToStorage")}", NotificationType.Success);
     }
 
     [RelayCommand]

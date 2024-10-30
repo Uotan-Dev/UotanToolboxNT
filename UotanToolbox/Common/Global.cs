b3149539bae18768163c43f3bfb7b3d0b1cb023b
mujianwu <lrh15563910083@163.com>
2024-10-30 19:06:11 +0800

    3.2.1

--- UotanToolbox/Common/Global.cs
+++ UotanToolbox/Common/Global.cs
@@ -30,7 +30,7 @@ namespace UotanToolbox.Common
         public static string sdftable = "";
         public static string emmcrom = "";
         //工具箱版本
-        public static string currentVersion = "3.2.0";
+        public static string currentVersion = "3.2.1";
         public static bool isLightThemeChanged = false;
         //主页的Dialog
         public static ISukiDialogManager MainDialogManager;

fab44cb7c308bd1dd5c80dea28e78ade8300a46e
mujianwu <lrh15563910083@163.com>
2024-11-08 23:19:24 +0800

    初步完成9008页面

---
+++ UotanToolbox/Features/EDL/EDLView.axaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+using Avalonia.Collections;
+using Avalonia.Controls;
+using Avalonia.Controls.Notifications;
+using Avalonia.Interactivity;
+using Avalonia.Platform.Storage;
+using SukiUI.Dialogs;
+using SukiUI.Toasts;
+using UotanToolbox.Common;
+using UotanToolbox.Common.PatchHelper;
+
+namespace UotanToolbox.Features.EDL;
+
+public partial class EDLView : UserControl
+{
+    private static string GetTranslation(string key)
+    {
+        return FeaturesHelper.GetTranslation(key);
+    }
+
+    public EDLView()
+    {
+        InitializeComponent();
+    }
+}
---
+++ UotanToolbox/Features/EDL/EDLViewModel.cs
@@ -0,0 +1,18 @@
+﻿using Material.Icons;
+using UotanToolbox.Common;
+
+
+namespace UotanToolbox.Features.EDL;
+
+public partial class EDLViewModel : MainPageBase
+{
+
+    private static string GetTranslation(string key)
+    {
+        return FeaturesHelper.GetTranslation(key);
+    }
+
+    public EDLViewModel() : base("9008刷机", MaterialIconKind.CableData, -350)
+    {
+    }
+}

0aa79ebf35cfb4282f58424497c3a5b018564df2
mujianwu <lrh15563910083@163.com>
2024-11-10 00:13:58 +0800

    初步完成文件管理页面

---
+++ UotanToolbox/Features/Filemgr/FilemgrView.axaml.cs
@@ -0,0 +1,31 @@
+﻿using Avalonia.Collections;
+using Avalonia.Controls;
+using Avalonia.Controls.Notifications;
+using Avalonia.Interactivity;
+using SukiUI.Dialogs;
+using System.Collections.ObjectModel;
+using System.IO;
+using System.Threading.Tasks;
+using UotanToolbox.Common;
+
+
+namespace UotanToolbox.Features.Filemgr;
+
+public partial class FilemgrView : UserControl
+{
+    private static string GetTranslation(string key)
+    {
+        return FeaturesHelper.GetTranslation(key);
+    }
+    public AvaloniaList<Node> TreeViewContent { get; } = [];
+    public FilemgrView()
+    {
+        InitializeComponent();
+        FileView.ItemsSource = new Project[10];
+    }
+
+    private async void Open(object sender, RoutedEventArgs args)
+    {
+        Global.MainDialogManager.CreateDialog().WithTitle(GetTranslation("Common_Error")).OfType(NotificationType.Error).WithContent(GetTranslation("Common_NotConnected")).Dismiss().ByClickingBackground().TryShow();
+    }
+}
---
+++ UotanToolbox/Features/Filemgr/FilemgrViewModel.cs
@@ -0,0 +1,49 @@
+﻿using Avalonia.Collections;
+using Avalonia.Controls.Notifications;
+using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using DynamicData;
+using Material.Icons;
+using SukiUI.Dialogs;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Threading.Tasks;
+using UotanToolbox.Common;
+using UotanToolbox.Features.Appmgr;
+
+namespace UotanToolbox.Features.Filemgr;
+
+public partial class FilemgrViewModel : MainPageBase
+{
+    private static string GetTranslation(string key)
+    {
+        return FeaturesHelper.GetTranslation(key);
+    }
+    public AvaloniaList<Node> TreeViewContent { get; } = [];
+
+    public FilemgrViewModel() : base("ROM工坊", MaterialIconKind.ChartPieOutline, -250)
+    {
+        TreeViewContent.AddRange(
+            Enumerable.Range(1, 10).Select(x => new Node($"Outer {x}",
+                Enumerable.Range(1, 5).Select(y => new Node($"Inner {y}",
+                    Enumerable.Range(1, 2).Select(z => new Node($"Innermost {z}",
+                        Enumerable.Range(1, 2).Select(n => new Node($"1234567 {n}")))))))));
+    }
+}
+
+public partial class Project : ObservableObject
+{
+    [ObservableProperty]
+    private string projectName;
+
+    [ObservableProperty]
+    private bool isSelected;
+}
+
+public partial class Node(string value, IEnumerable<Node>? subNodes = null) : ObservableObject
+{
+    public AvaloniaList<Node> SubNodes { get; } = new(subNodes ?? Array.Empty<Node>());
+    [ObservableProperty] private string _value = value;
+}

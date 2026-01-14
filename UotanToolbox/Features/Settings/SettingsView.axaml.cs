434293bc35c682e02df2b48f213a57ce9e3f4118
暮间雾 <lrh15563910083@163.com>
2026-01-14 23:19:37 +0800

    完成修补配置文件选择

--- UotanToolbox/Features/Settings/SettingsView.axaml.cs
+++ UotanToolbox/Features/Settings/SettingsView.axaml.cs
@@ -1,5 +1,9 @@
 ﻿using Avalonia.Controls;
+using Avalonia.Interactivity;
 using Avalonia.Markup.Xaml;
+using Avalonia.Platform.Storage;
+using SkiaSharp;
+using UotanToolbox.Common;
 
 namespace UotanToolbox.Features.Settings;
 
@@ -13,5 +17,27 @@ public partial class SettingsView : UserControl
     private void InitializeComponent()
     {
         AvaloniaXamlLoader.Load(this);
+    }
+
+    private static FilePickerFileType CsvPicker { get; } = new("CSV File")
+    {
+        Patterns = new[] { "*.csv" },
+        AppleUniformTypeIdentifiers = new[] { "*.csv" }
+    };
+
+    private async void OpenCSVFile(object sender, RoutedEventArgs args)
+    {
+        TopLevel topLevel = TopLevel.GetTopLevel(this);
+        System.Collections.Generic.IReadOnlyList<IStorageFile> files = await topLevel.StorageProvider.OpenFilePickerAsync(new FilePickerOpenOptions
+        {
+            Title = "Open File",
+            AllowMultiple = false,
+            FileTypeFilter = new[] { CsvPicker }
+        });
+        if (files.Count >= 1)
+        {
+            CSVPath.Text = files[0].TryGetLocalPath();
+            Global.BootPatchPath = CSVPath.Text;
+        }
     }
 }

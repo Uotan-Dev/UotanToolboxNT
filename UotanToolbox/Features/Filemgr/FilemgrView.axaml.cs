759c4b37bd643d3c565654ef4dfcc3bf7ae6e8c3
暮间雾 <lrh15563910083@163.com>
2026-06-10 17:26:27 +0800

    Add Filemgr feature UI, viewmodel, assets
    Introduce a new File Manager feature: add FilemgrView.axaml (UI) with placeholder layout and context menus, FilemgrView.axaml.cs (code-behind) with an Open handler that shows a not-connected notification, and FilemgrViewModel.cs which defines the Filemgr viewmodel and a Project model. Add two asset icons (Assets/file.png, Assets/folder.png). Minor formatting/indentation adjustment in App.axaml. Update UotanToolbox.csproj to register the new view and add the Tmds.DBus.Protocol package reference.

---
+++ UotanToolbox/Features/Filemgr/FilemgrView.axaml.cs
@@ -0,0 +1,26 @@
+﻿using Avalonia.Collections;
+using Avalonia.Controls;
+using Avalonia.Controls.Notifications;
+using Avalonia.Interactivity;
+using SukiUI.Dialogs;
+using UotanToolbox.Common;
+
+
+namespace UotanToolbox.Features.Filemgr;
+
+public partial class FilemgrView : UserControl
+{
+    private static string GetTranslation(string key)
+    {
+        return FeaturesHelper.GetTranslation(key);
+    }
+    public FilemgrView()
+    {
+        InitializeComponent();
+    }
+
+    private async void Open(object sender, RoutedEventArgs args)
+    {
+        Global.MainDialogManager.CreateDialog().WithTitle(GetTranslation("Common_Error")).OfType(NotificationType.Error).WithContent(GetTranslation("Common_NotConnected")).Dismiss().ByClickingBackground().TryShow();
+    }
+}
---
+++ UotanToolbox/Features/Filemgr/FilemgrViewModel.cs
@@ -0,0 +1,34 @@
+﻿using Avalonia.Collections;
+using CommunityToolkit.Mvvm.ComponentModel;
+using Material.Icons;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using UotanToolbox.Common;
+using UotanToolbox.Features.Appmgr;
+
+namespace UotanToolbox.Features.Filemgr;
+
+public partial class FilemgrViewModel : MainPageBase
+{
+    [ObservableProperty]
+    private ObservableCollection<File> files = [];
+    private static string GetTranslation(string key)
+    {
+        return FeaturesHelper.GetTranslation(key);
+    }
+
+    public FilemgrViewModel() : base("文件管理", MaterialIconKind.FolderOutline, -625)
+    {
+    }
+}
+
+public partial class File : ObservableObject
+{
+    [ObservableProperty]
+    private string fileName;
+
+    [ObservableProperty]
+    private string fileTime;
+}
